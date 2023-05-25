2cb87b0e1819d960a4c0b5ab9de7bdc5455515da
whitebear05 <woojs0505@naver.com>
2023-05-25 10:57:22 +0900

    :memo: :: ToDo category 적용

---
+++ todo-app/src/components/ToDo.tsx
@@ -0,0 +1,43 @@
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
+
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo: IToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
+
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
+}
+
+export default ToDo;
---
+++ todo-app/src/components/ToDoList.tsx
@@ -0,0 +1,24 @@
+import { useRecoilValue } from "recoil";
+import CreateToDo from "./CreateToDo";
+import { toDoState } from "../atoms";
+import ToDo from "./ToDo";
+
+function ToDoList() {
+  const toDos = useRecoilValue(toDoState);
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+
+      <CreateToDo />
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
